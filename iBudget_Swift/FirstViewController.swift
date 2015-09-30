33b3fd973019482ff67219bb2d02e77d81ca89e9
Alex Ling <hkalexling@gmail.com>
2015-09-30 22:14:22 +0800

    Initial Commit

---
+++ iBudget_Swift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iBudget_Swift
+//
+//  Created by Alex Ling on 30/9/2015.
+//  Copyright © 2015 Alex Ling. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ iBudget_SwiftTests/iBudget_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  iBudget_SwiftTests.swift
+//  iBudget_SwiftTests
+//
+//  Created by Alex Ling on 30/9/2015.
+//  Copyright © 2015 Alex Ling. All rights reserved.
+//
+
+import XCTest
+@testable import iBudget_Swift
+
+class iBudget_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
